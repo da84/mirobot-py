a6f3b5e2343a1c61872968fc8ed5d58c3b1198f7
Dennis Armbruster <Dennis.Armbruster@live.de>
2021-08-07 16:49:52 +0200

    tests for running it easier in IDE

---
+++ test.py
@@ -0,0 +1,14 @@
+from mirobot import MirobotServer
+"""
+r = Mirobot(debug=False, ewait=True)
+print("home...", end="")
+r.home_simultaneous()
+print("finished")
+print("zero...", end="")
+r.go_to_zero()
+print("finished")
+print("home...", end="")
+r.home_simultaneous()
+print("finished")
+"""
+s = MirobotServer()
